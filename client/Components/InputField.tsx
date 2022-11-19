7146a5e010e573ce05d122de60b445bc6012c970
aseeralfaisal <aseeralfaisal@gmail.com>
2022-11-19 21:46:37 +0600

    sign up sign in front end updated

--- client/Components/InputField.tsx
+++ client/Components/InputField.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from '../styles/Inputfield.module.scss';
 
-const InputField = ({ placeholder }) => {
+const InputField = ({ placeholder, setValue }: { placeholder: string; setValue: Function }) => {
   return (
     <div>
-      <input className={styles.input} type='text' placeholder={placeholder} />
+      <input
+        className={styles.input}
+        type='text'
+        placeholder={placeholder}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   );
 };
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import Button1 from '../Components/Button1';
 import InputField from '../Components/InputField';
 import styles from '../styles/Home.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   const [registerMode, setRegisterMode] = useState(false);
   const changeMode = () => setRegisterMode(!registerMode);
+  const [userValue, setUserValue] = useState('');
+  const [passValue, setPassValue] = useState('');
 
-  const SignUpDesc = ({ desc, action }) => {
+  const LogDescription = ({ desc, action }) => {
     return (
       <div className={styles.register}>
         <label className={styles.desc}>{desc}</label>
@@ -22,14 +26,20 @@ export default function Home() {
     <>
       <div className={styles.form}>
         <div className={styles.form__child}>
-          <InputField placeholder='Username' />
-          <InputField placeholder='Password' />
-          {registerMode ? <Button1 title='Register' /> : <Button1 title='Log in' />}
-          {!registerMode ? (
-            <SignUpDesc desc='Not Registered?' action='Register' />
-          ) : (
-            <SignUpDesc desc='Already have an account?' action='Sign in' />
-          )}
+          <div style={{ margin: 60 }}>
+            <FontAwesomeIcon icon={faCommentDots} color='#7c5cfc' fontSize={100} />
+            <h2 className={styles.textus}>Textus</h2>
+          </div>
+          <div>
+            <InputField placeholder='Username' setValue={setUserValue} />
+            <InputField placeholder='Password' setValue={setPassValue} />
+            {registerMode ? <Button1 title='Register' /> : <Button1 title='Log in' />}
+            {!registerMode ? (
+              <LogDescription desc='Not Registered?' action='Register' />
+            ) : (
+              <LogDescription desc='Already have an account?' action='Sign in' />
+            )}
+          </div>
         </div>
       </div>
     </>
