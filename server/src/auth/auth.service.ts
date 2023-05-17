246e5725184583ea65f348bab910ee850b8d3110
aseeralfaisal <aseeralfaisal@gmail.com>
2023-05-17 17:24:28 +0600

    removed constructor

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,26 +3,24 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  private readonly accessToken: string;
-  private readonly refreshToken: string;
-  private readonly jwtService: JwtService;
+  private static readonly accessToken: string = process.env.ACCESS_TOKEN || 'default_access_token';
+  private static readonly refreshToken: string = process.env.REFRESH_TOKEN || 'default_refresh_token';
+  private static readonly jwtService: JwtService = new JwtService({
+    privateKey: AuthService.accessToken,
+  });
 
-  constructor() {
-    this.accessToken = process.env.ACCESS_TOKEN;
-    this.refreshToken = process.env.REFRESH_TOKEN;
-    this.jwtService = new JwtService({
-      privateKey: this.accessToken,
-    });
+  static generateAccessToken(username: string): string {
+    return AuthService.jwtService.sign({ username }, { expiresIn: '1d' });
   }
 
-  generateAccessToken(username: string) {
-    return this.jwtService.sign({ username }, { expiresIn: '1d' });
-  }
-
-  generateRefreshToken(username: string) {
+  static generateRefreshToken(username: string): string {
     const refreshTokenService = new JwtService({
-      privateKey: this.refreshToken,
+      privateKey: AuthService.refreshToken,
     });
     return refreshTokenService.sign({ username }, { expiresIn: '30m' });
+  }
+
+  static verifyToken(token: string): any {
+    return AuthService.jwtService.verify(token);
   }
 }
