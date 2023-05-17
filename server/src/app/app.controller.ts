39c6b286a94f554ac55902324f98d1d1d4b03b91
aseeralfaisal <aseeralfaisal@gmail.com>
2023-05-17 17:17:48 +0600

    cleanup

--- server/src/app/app.controller.ts
+++ server/src/app/app.controller.ts
@@ -4,31 +4,37 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Post('/getchat')
-  getChat(@Body() data: { room: string }): object {
+  getChat(@Body() data: { room: string }) {
     return this.appService.getChat(data);
   }
+
   @Get('/getchatroom')
   getChatRoom() {
     return this.appService.getChatRoom();
   }
+
   @Post('/createchatroom')
   createChatRoom(@Body() data: { room: string }) {
     return this.appService.createChatRoom(data);
   }
+
   @Post('/login')
   login(@Body() data: { username: string; password: string }) {
     return this.appService.login(data);
   }
+
   @Post('/register')
   register(@Body() data: { username: string; password: string }) {
     return this.appService.registerUser(data);
   }
+
   @Get('/users')
   getUsers() {
     return this.appService.getUsers();
