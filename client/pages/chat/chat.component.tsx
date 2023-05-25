7cfd42a3d92c89d716309381902d2fcf09bf50e9
aseeralfaisal <aseeralfaisal@gmail.com>
2023-05-26 02:50:40 +0600

    fixes

--- client/pages/chat/chat.component.tsx
+++ client/pages/chat/chat.component.tsx
@@ -188,7 +188,6 @@ const ChatPage: React.FC<ChatType> = (props) => {
             fontSize={15}
             value={messageValue}
             setValue={setMessageValue}
-            reduxValue={false}
             event={sendMessageAction}
             startIcon={<Uicons.UilKeyboard color={colors.gray} size='20' />}
           />
