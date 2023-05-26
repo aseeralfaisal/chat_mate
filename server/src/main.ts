6c70988b035c5f609cfa9691e7dc9095e4e463cb
aseeralfaisal <aseeralfaisal@gmail.com>
2023-05-26 11:27:33 +0600

    updated

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: 'https://chatmate-dw88.onrender.com',
+    origin: '*',
   });
   app.use(cookieParser());
   app.use(
