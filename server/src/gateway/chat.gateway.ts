3c9ce6734e3d79b622b610b380c62286c1729257
aseeralfaisal <aseeralfaisal@gmail.com>
2023-05-26 11:46:37 +0600

    allowheaders*

--- server/src/gateway/chat.gateway.ts
+++ server/src/gateway/chat.gateway.ts
@@ -11,7 +11,8 @@ const prisma = new PrismaClient();
 
 @WebSocketGateway({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
+    allowedHeaders: '*',
   },
 })
 export class EventsGateway {
